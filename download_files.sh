a93a1d7e23bf230b15f87592dd9654da4bf37273
prashantg123 <pg0408@gmail.com>
2023-12-11 15:56:06 +0800

    dockercompose  and dockerfile.spark

---
+++ download_files.sh
@@ -0,0 +1,2 @@
+scp -i azure-key-pair.cer -rp azureuser@20.212.233.247:/home/azureuser/output src/output
+
---
+++ upload_files.sh
@@ -0,0 +1,1 @@
+scp -i azure-key-pair.cer -r ./src/* azureuser@20.212.233.247:/home/azureuser
